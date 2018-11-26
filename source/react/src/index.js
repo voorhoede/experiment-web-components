9271530a18dee63074343cac9bdafa40f474b7e1
Vincent van Dijck <vincent@voorhoede.nl>
2018-11-26 14:23:17 +0100

    feat(src) create view and component

---
+++ source/react/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Experiment from './views/experiment/experiment';
+
+ReactDOM.render(<Experiment />, document.getElementById('root'));
