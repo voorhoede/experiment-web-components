1760c59c8e728d876f9288768f165703cc2e7e72
Vincent van Dijck <vincent@voorhoede.nl>
2018-11-27 11:26:29 +0100

    feat(color-picker) export all different components

--- source/react/src/app.js
+++ source/react/src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import './color-picker';
+import ColorPicker from './color-picker';
 
 const App = () => (
-  <color-picker></color-picker>
+  <div>
+    <h2>React components</h2>
+    <ColorPicker />
+    <ColorPicker hue={180} />
+    <ColorPicker>
+      <input type="number" min="0" max="360" step="1" />
+    </ColorPicker>
+
+    <h2>Web components</h2>
+    <color-picker></color-picker>
+    <color-picker-with-prop></color-picker-with-prop>
+    <color-picker-with-child></color-picker-with-child>
+  </div>
 );
 
 export default App;
