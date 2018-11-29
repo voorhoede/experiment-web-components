e70fbd93d4df7897953576e3baa0f968f5e1e7f7
Vincent van Dijck <vincent@voorhoede.nl>
2018-11-29 11:43:39 +0100

    feat (react) use single web component

--- source/react/src/app.js
+++ source/react/src/app.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import ColorPicker from './color-picker';
+import './color-picker';
 
 const App = () => (
-  <div>
-    <h2>React components</h2>
-    <ColorPicker />
-    <ColorPicker hue={180} />
-    <ColorPicker>
-      <input type="number" min="0" max="360" step="1" />
-    </ColorPicker>
-  </div>
+  <div></div>
 );
 
 export default App;
